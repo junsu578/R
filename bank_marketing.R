a72883a883ac0bcdfcab42dfc36fce42e71cd9f5
junsu.jeon <junsu578@gmail.com>
2020-03-20 17:39:23 +0900

    practice

---
+++ bank_marketing.R
@@ -0,0 +1,21 @@
+library(usethis)
+
+use_github()
+
+
+library(ggplot2)
+library(ggthemes)
+library(class)
+library(caret)
+library(randomForest)
+library(dplyr)
+library(mlr)
+library(xgboost)
+library(data.table)
+library(DataExplorer)
+library(plotly)
+library(tidyverse)
+
+
+bank<-read.csv("C:/Users/User/Documents/데이터/bank-additional_bank-additional-full.csv", header=T)
+
